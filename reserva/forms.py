69e6e5d02a3097ba5770b99895e5add8f98366ca
William Santos <Willsantos.edf@gmail.com>
2023-01-23 15:16:08 -0300

    implementando restrições.

--- reserva/forms.py
+++ reserva/forms.py
@@ -4,26 +4,21 @@ from datetime import date
 
 class ReservaForm(forms.ModelForm):
 
-    def clean_data(self):
-        data = self.cleaned_data['data']
-        hoje = date.today()
-        if data < hoje:
-            raise forms.ValidationError('Não é possivel realizar uma reserva para o passado!')
-        return data
-
     def clean_date(self):
-        reserva = self.cleaned_data['data']
-        if reserva.count() >= 14:
-            raise forms.ValidationError('Não temos mais vagas para esta data!')
-        else:
-            return reserva
+        form_date = self.cleaned_data["data"]
+        today_date = date.today()
+        if form_date < today_date:
+            raise forms.ValidationError('OPS, a data informada é anterior a data de hoje')
+        return form_date
 
-    """ def clean_date(self):
-        reserva = Reserva.objects.filter(data__day=16).count()
-        if reserva >= 4:
-            raise forms.ValidationError('Não temos mais vagas para esta data!')
-        return  """
- 
+    def clean_shift(self):
+        form_date = self.cleaned_data["date"]
+        form_shift = self.cleaned_data["turno"]
+        baths_in_same_day_and_hour = Reserva.objects.filter(date=form_date, shift=form_shift).count()
+        if baths_in_same_day_and_hour >= 4:
+            raise forms.ValidationError(f'Ops esse horário já esta lotado, para o turno da {form_shift}')
+        return form_shift
+
 
     class Meta:
         model = Reserva
@@ -31,3 +26,25 @@ class ReservaForm(forms.ModelForm):
             'nome', 'email', 'nome_pet', 'data', 'turno',
              'tamanho', 'observacoes'
         ]
+    
+
+""" class ReservationForm(forms.ModelForm):
+
+    def clean_date(self):
+        form_date = self.cleaned_data["date"]
+        today_date = date.today()
+        if form_date < today_date:
+            raise forms.ValidationError('OPS, a data informada é anterior a data de hoje')
+        return form_date
+
+    def clean_shift(self):
+        form_date = self.cleaned_data["date"]
+        form_shift = self.cleaned_data["shift"]
+        baths_in_same_day_and_hour = Reservation.objects.filter(date=form_date, shift=form_shift).count()
+        if baths_in_same_day_and_hour >= 4:
+            raise forms.ValidationError(f'Ops esse horário já esta lotado, para o turno da {form_shift}')
+        return form_shift
+
+    class Meta:
+        model = Reservation
+        fields = "all" """
