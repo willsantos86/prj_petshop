b7179b60e711497e2212f031c460568ee31e99de
William Santos <Willsantos.edf@gmail.com>
2023-01-27 11:25:57 -0300

    teste

--- rest_api/management/commands/create_token.py
+++ rest_api/management/commands/create_token.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand,Token, User
 
 class Command(BaseCommand):
     help = 'Cria um novo token para ser usado'
